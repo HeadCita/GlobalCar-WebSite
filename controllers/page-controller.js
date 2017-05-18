12dd3ee387b7f39df097c11eb3d40ee4b3f7d0bb
Joseph León <leonus96@gmail.com>
2017-05-18 00:13:26 -0500

    Meta titles y meta description

--- controllers/page-controller.js
+++ controllers/page-controller.js
@@ -4,56 +4,97 @@
 "use strict";
 class PageController{
 	getHome(req, res, next){
-		res.render('home');
+		res.render('home',{
+			title: "Global Car: escuela de manejo para obtener tu brevete.",
+			description: "Global Car es tu mejor opción como escuela de manejo y te brinda asesoramiento total para el trámite de tu brevete."
+		});
 	}
 	getUs(req, res, next){
-		res.render('nosotros');
+		res.render('nosotros',{
+			title: "Global Car| Nosotros",
+			description: "Somos una empresa especializada en ensearte a manejar."
+		});
 	}
 	//----COURSES-->
 	getCursoNuevoAi(req, res, next){
-		res.render('cursos/nuevos-ai');
+		res.render('cursos/nuevos-ai',{
+			title: "Global Car | Brevetes AI",
+			description: "El curso con el que debes empezar para aprender a conducir y obtener tu brevete."
+		});
 	}
 	getCursoRecategorizacion(req, res, next){
-		res.render('cursos/recategorizacion');
+		res.render('cursos/recategorizacion', {
+			title: "Global Car | Recategorización",
+			description: "Asciende de categoría con este curso."
+		});
 	}
 	getCursoSoloManejo(req, res, next){
-		res.render('cursos/solo-manejo');
+		res.render('cursos/solo-manejo',{
+			title: "Global Car | Sólo manejo",
+			description: "Aprende a conducir como un profesional."
+		});
 	}
 	getCursoManejoDefensivo(req, res, next){
-		res.render('cursos/manejo-defensivo');
+		res.render('cursos/manejo-defensivo', {
+			title: "Global Car | Manejo Defensivo",
+			description: "Capacítate con los mejores, aprende todo lo necesario para conducir seguro."
+		});
 	}
 	getCursoCamioneta(req, res, next){
-		res.render('cursos/camioneta');
+		res.render('cursos/camioneta', {
+			title: "Global Car | Camioneta 4x4",
+			description: "Aprende a conducir camionetas 4x4 como los mejores."
+		});
 	}
 	//----CATEGORIES-->
 	getCategoryAi(req, res, next){
-		res.render('categories/ai');
+		res.render('categories/ai', {
+			title: "Global Car | Categoría AI",
+			description: "Primera categoría de brevetes."
+		});
 	}
 	getCategoryAiia(req, res, next){
-		res.render('categories/aiia');
+		res.render('categories/aiia', {
+			title: "Global Car | Catgoría AIIa",
+			description: "Aprende sobre la categoría AIIa."
+		});
 	}
 	getCategoryAiib(req, res, next){
-		res.render('categories/aiib');
+		res.render('categories/aiib', {
+			title: "Global Car | Catgoría AIIb",
+			description: "Aprende sobre la categoría AIIb."
+		});
 	}
 	getCategoryAiiia(req, res, next){
-		res.render('categories/aiiia');
+		res.render('categories/aiiia', {
+			title: "Global Car | Catgoría AIIIa",
+			description: "Aprende sobre la categoría AIIIa."
+		});
 	}
 	getCategoryAiiib(req, res, next){
-		res.render('categories/aiiib');
+		res.render('categories/aiiib', {
+			title: "Global Car | Catgoría AIIIb",
+			description: "Aprende sobre la categoría AIIIb."
+		});
 	}
 	getCategoryAiiic(req, res, next){
-		res.render('categories/aiiic');
+		res.render('categories/aiiic', {
+			title: "Global Car | Catgoría AIIIc",
+			description: "Aprende sobre la categoría AIIIc."
+		});
 	}
-	getLocales(req, res, next){
-		res.render('locales');
-	}
-
 	getContacto(req, res, next){
-		res.render('contacto');
+		res.render('contacto', {
+			title: "Global Car | Contacto",
+			description: "Contáctanos y decide por tu brevete!."
+		});
 	}
 
 	getGaleria(req, res, next){
-		res.render('galery');
+		res.render('galery', {
+			title: "Global Car | Galería",
+			description: "Ellos ya saben conducir, solo faltas tu!"
+		});
 	}
 }
 
